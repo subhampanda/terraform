61afecce6e1c6378ce113d066b0250ab0fb816db
subhampanda <panda.subham4@gmail.com>
2021-10-26 14:47:17 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-  access_key = "AKIAUIMJ4HWFRL7HXYTD"
-  secret_key = "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"
+  access_key = "AKIAUIMJ4HWF5737U2NX"
+  secret_key = "o+QM7j79tv46D0+sJp80hNi+CQZKjHqdqfV7re4v"
   region     = "us-east-2"
 }
 resource "aws_instance" "example" {
